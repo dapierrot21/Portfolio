7bda814820d285715d63a1f17399f01e93aa022c
Dean Pierrot <dapierrot21@gmail.com>
2017-08-09 20:44:27 -0400

    added files

---
+++ Portfolio-master/js/main.js
@@ -0,0 +1,30 @@
+// nav dropdown menu
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+// scroll attached to nav links
+$(function() {
+  $('.js-nav a').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
+});
+
+//Showing and hiding
+
+  $(".main-heading").hide().fadeIn(2300);
